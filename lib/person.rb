ce05ce033482866969719fc404392a42c037fd66
+ <+dannaewaltz@gmail.com>
2022-08-14 10:20:00 -0600

    commit tree 55fb805229fdf397528e49cac70a6a5c023ef978

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
-
+    #setter method has parameter, = identifies this as the setter method it is not an equal sign
+    def name=(name)
+        #now the = sign is an operator
+        @name = name
+    end
+    #getter method
+    def name
+        @name
+    end
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
+
+#any_object.any_method(arg)
